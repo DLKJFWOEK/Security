7fc0ba7cce948cb70663bb7f72ec368b001554b6
resilience <frog4821@naver.com>
2024-01-27 17:24:01 +0900

    login.html(Thymeleaf 사용) 대신 login2.html로 임시 사용, login2.html에서 layout.html(Thymeleaf 사용) 으로 redirect 안됨)

--- Bulletin_board/src/main/java/edu/fisa/board/controller/LoginController.java
+++ Bulletin_board/src/main/java/edu/fisa/board/controller/LoginController.java
@@ -30,14 +30,16 @@ public class LoginController {
     
     @PostMapping("/login")
     public String login(@RequestParam String username, @RequestParam String password, Model model){
+    	System.out.println("0000 ---  " + username + " " + password);
     	// 사용자 자격 증명 검증 로직
     	if(authenticationService.isValidCredentials(username, password)) {
     		//로그인 성공
-    		return "redirect:/board/layout";
+    		System.out.println("++++++++++++++");
+    		return "redirect:layout";   //?
         } else {
             // 로그인 실패 시 로그인 페이지로 이동
-            model.addAttribute("error", "자격증명에 실패하였습니다.");
-            return "login";
+            model.addAttribute("error", "자격 증명에 실패하였습니다.");
+            return "redirect:login";
         }
     }
 }
