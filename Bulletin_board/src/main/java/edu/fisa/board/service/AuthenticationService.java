8eec92a316588a5d829f6862e4fb49d2465886cb
hwiju yeom <yeom456@gmail.com>
2024-01-31 17:52:37 +0900

    feat: 로그인 체크 기능

--- Bulletin_board/src/main/java/edu/fisa/board/service/AuthenticationService.java
+++ Bulletin_board/src/main/java/edu/fisa/board/service/AuthenticationService.java
@@ -1,12 +1,33 @@
 package edu.fisa.board.service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
+import edu.fisa.board.client.domain.entity.Client;
+import edu.fisa.board.client.persistence.ClientRepository;
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class AuthenticationService {
-	
+
+	private final ClientRepository clientRepository;
+
 	public boolean isValidCredentials(String username, String password) {
-		
-		return true;
+
+		Optional<Client> op = clientRepository.findByLoginId(username);
+
+		if (op.isPresent()) {
+
+			Client client = op.get();
+
+			if (client.getPwd().equals(password) && client.getLoginId().equals(username)){
+
+				return true;
+			}
+		}
+
+		return false;
 	}
 }
